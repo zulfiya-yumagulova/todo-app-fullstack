23bf5d2c03099cc4f3cc12072ed07160ad3ece22
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-06-30 14:26:14 +0100

    fixed CORS error

--- server/app.js
+++ server/app.js
@@ -5,6 +5,16 @@ import todosRouter from "./routes/todos.js";
 
 const app = express();
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use("/todos", todosRouter);
